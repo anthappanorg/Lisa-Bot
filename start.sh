ea551d59cbac67f07df9517d09eff20a9f347d6f
Annuay Raj-k <100431877+anthappanorg@users.noreply.github.com>
2022-12-13 18:59:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CodeHouse-TG/Lisa-Bot.git /Lisa-Bot
+  git clone https://github.com/anthappanorg/Lisa-Bot.git /Lisa-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Lisa-Bot
 fi
 cd /Lisa-Bot
 pip3 install -U -r requirements.txt
-echo "Starting LIsa-Bot...."
+echo "Starting LIsa Bot...."
 python3 bot.py
